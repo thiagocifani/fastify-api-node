bebbfd8e5e5ad03780095f6b1ac70b0d0bc972b6
thiagocifani <cifani.thiago@gmail.com>
2023-10-27 04:18:43 -0300

    Add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,27 @@
 import fastify from 'fastify'
+import { env } from './env'
+// import crypto from 'node:crypto'
+import { knex } from './database'
 
 const app = fastify({ logger: true })
 
-app.get('/', () => {
-  return { hello: 'world' }
+app.get('/', async () => {
+  // const transaction = await knex('transactions').insert({
+  //   id: crypto.randomUUID(),
+  //   title: 'Salary',
+  //   amount: 3000,
+  // })
+
+  const transactions = await knex('transactions')
+    .where('amount', 3000)
+    .select('*')
+
+  return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('Server is running on port 3333')
