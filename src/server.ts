f9cce69e75373d7e825761ca73a4a58075505b4c
thiagocifani <cifani.thiago@gmail.com>
2023-10-27 03:11:12 -0300

    Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from 'fastify'
+
+const app = fastify({ logger: true })
+
+app.get('/', () => {
+  return { hello: 'world' }
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
