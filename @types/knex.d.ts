533a81f5317ec4b4eb16a711b0783274299c920f
thiagocifani <cifani.thiago@gmail.com>
2023-10-31 00:59:22 -0300

    Add type to transactions

---
+++ @types/knex.d.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      create_at: string
+      type: 'credit' | 'debit'
+    }
+  }
+}
